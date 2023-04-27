1155533b46b501e59108705457d1a244c5118c0b
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-04-27 19:49:46 +0530

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const replaceThis = "monday";
 const replacewith = "thanos";
-const folder = __dirname
+const folder = __dirname;
 
 try {
   fs.readdir(".", (err, data) => {
     for (let index = 0; index < data.length; index++) {
       const item = data[index];
-      let oldfile = path.join(folder, item)
-      let newfile = path.join(folder, item.replace(replaceThis, replacewith))
+      let oldfile = path.join(folder, item);
+      let newfile = path.join(folder, item.replace(replaceThis, replacewith));
       fs.rename(oldfile, newfile, () => {
         console.log("Rename Success");
       });
