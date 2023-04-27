7356ff42b7f25e6904d419ae08877916f97d4abd
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-04-27 19:40:18 +0530

    project is completed according to the video but from my side it is still not completed it is working but donot as the shell script

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const path = require('path');
 const replaceThis = "monday";
 const replacewith = "thanos";
 const preview = true;
-const folder = path(__dirname);
+const folder = __dirname
 
 try {
   fs.readdir(folder, (err, data) => {
     for (let index = 0; index < data.length; index++) {
       const item = data[index];
       let oldfile = path.join(folder, item)
-      let newfile = "" + item.replace(replaceThis, replacewith);
+      let newfile = path.join(folder, item.replace(replaceThis, replacewith))
       if (!preview) {
-        fs.rename(, newfile, () => {
+        fs.rename(oldfile, newfile, () => {
           console.log("Rename Success");
         });
       } else {
--- rename.sh
+++ rename.sh
@@ -1,1 +1,7 @@
-node index.js
+# !/bin/bash
+
+script_full_path=$(dirname "$0")
+
+echo $script_full_path
+echo "node $script_full_path/index.js
+"node "$script_full_path/index.js"
